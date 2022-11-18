03a466e20fde39ca996b8d1eda73beccaa0b7cb4
Joel Ano <joelano6738@gmail.com>
2022-11-18 17:18:11 +0100

    creation des differents element composant ma fenetre

---
+++ java-interface graphique/MaFenetre.java
@@ -0,0 +1,14 @@
+
+package methode;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class MaFenetre extends JFrame{
+    JLabel Jbl = new JLabel("Nom:");
+    JTextField Jtf = new JTextField(12);
+    JButton Jbt = new JButton("add");
+    
+}
