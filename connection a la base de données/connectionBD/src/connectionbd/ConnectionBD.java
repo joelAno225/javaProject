983630edc3a26e17440171cfb021baacc7c99bb0
Joel Ano <joelano6738@gmail.com>
2022-11-21 16:04:45 +0100

    connection Bd

--- connection a la base de données/connectionBD/src/connectionbd/ConnectionBD.java
+++ connection a la base de données/connectionBD/src/connectionbd/ConnectionBD.java
@@ -5,13 +5,13 @@ import java.sql.*;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
+import java.sql.*;
 public class ConnectionBD {
 
     public static void main(String[] args) {
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/bd_test", "root", "");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/bd", "root", "");
             Statement st = conn.createStatement();
             ResultSet rs = st.executeQuery("select * from Produit");
             while (rs.next()) {
@@ -19,7 +19,7 @@ public class ConnectionBD {
                 
             }
         } catch (Exception ex) {
-            Logger.getLogger(ConnectionBD.class.getName()).log(Level.SEVERE, null, ex);
+           ex.printStackTrace();
         }
     }
     
